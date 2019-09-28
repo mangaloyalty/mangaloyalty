319ad84c3e53ed7cd23e9eaabb31ddced0637103
Roel van Uden <r.h.g.v.uden@outlook.com>
2019-09-28 09:45:50 +0200

    Bump versions, README, and server

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ This project is in **PREVIEW**. Instructions may be unclear.
 
 ## Prerequisites
 
-* `NodeJS` >= `10.x` (http://nodejs.org/)
-* `NPM` >= `4.x` (https://www.npmjs.org/)
+* `NodeJS` >= `12.x` (http://nodejs.org/)
+* `NPM` >= `6.x` (https://www.npmjs.org/)
 
 ## Installation
 
--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-const http = require('http');
-const server = require('express')();
-
-// Initialize the server.
+const express = require('express');
+const server = express();
 server.disable('x-powered-by');
 server.use(require('mangaloyalty-client'));
 server.use(require('mangaloyalty-server'));
-
-// Initialize the server sockets.
-const p1 = new Promise((resolve) => http.createServer(server).listen(7767, resolve));
-const p2 = new Promise((resolve) => http.createServer(server).listen(7783, resolve))
-Promise.all([p1, p2]).then(() => console.log('Running on http://localhost:7783/'));
+server.listen(7783, () => console.log(`Running on http://localhost:7783/`));
