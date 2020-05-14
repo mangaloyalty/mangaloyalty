528cc35fc52c8cedaee0107866208fbbf94c72ed
Deathspike <meister.deathspike@outlook.com>
2020-05-14 21:55:48 +0200

    Fix electron windows publish

--- travis/builder-wine.sh
+++ travis/builder-wine.sh
@@ -3,4 +3,4 @@
     -v "${PWD}":/project \
     -v ~/.cache/electron:/root/.cache/electron \
     -v ~/.cache/electron-builder:/root/.cache/electron-builder \
-    electronuserland/builder:wine /bin/bash -c "npm ci && npm run release:wine"
+    electronuserland/builder:wine /bin/bash -c "npm ci && npm run release -- --windows"
